144b08ec164ce9c34fe9d107fcfa9a27eaeb0e10
Basil <basiltalias92@gmail.com>
2016-11-17 13:34:11 -0500

    Added Token interface

---
+++ src/main/java/matrians/instapaysam/TokenList.java
@@ -0,0 +1,11 @@
+package matrians.instapaysam;
+
+import com.stripe.android.model.Token;
+
+/**
+ * Created by basil on 2016-11-17.
+ */
+
+public interface TokenList {
+        public void addToList(Token token);
+}
