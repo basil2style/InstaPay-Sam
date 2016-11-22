5d2a7a005b514e0a9f7ecce22d75a27a8308c775
Basil <basiltalias92@gmail.com>
2016-11-22 12:49:34 -0500

    Added event card form save and card number listener for validating card number

--- src/main/java/matrians/instapaysam/Payment/AddPayment.java
+++ src/main/java/matrians/instapaysam/Payment/AddPayment.java
@@ -2,16 +2,26 @@ package matrians.instapaysam.Payment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.braintreepayments.cardform.OnCardFormSubmitListener;
 import com.braintreepayments.cardform.view.CardForm;
+import com.stripe.android.Stripe;
+import com.stripe.android.TokenCallback;
+import com.stripe.android.model.Card;
+import com.stripe.android.model.Token;
+import com.stripe.exception.AuthenticationException;
 
 import matrians.instapaysam.R;
+
+import static matrians.instapaysam.Payment.PaymentPage.PUBLISHABLE_KEY;
 
 public class AddPayment extends AppCompatActivity {
 
     private CardForm cardForm;
-    private Button checkout_button;
+    private Button save_button;
 
 
     @Override
@@ -20,7 +30,7 @@ public class AddPayment extends AppCompatActivity {
         setContentView(R.layout.activity_add_payment);
 
         cardForm = (CardForm) findViewById(R.id.bt_card_form);
-        checkout_button = (Button) findViewById(R.id.checkout_card);
+        save_button = (Button) findViewById(R.id.save_card);
 
         cardForm.cardRequired(true)
                 .expirationRequired(true)
@@ -28,5 +38,52 @@ public class AddPayment extends AppCompatActivity {
                 .actionLabel("Save")
                 .setup(this);
 
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cardForm.isValid()) {
+                    getStripeToken(cardForm.getCardNumber(), cardForm.getExpirationMonth(), cardForm.getExpirationYear(), cardForm.getCvv());
+                    
+                } else {
+                    cardForm.validate();
+                }
+            }
+        });
+        cardForm.setOnCardFormSubmitListener(new OnCardFormSubmitListener() {
+            @Override
+            public void onCardFormSubmit() {
+                if (cardForm.isValid()) {
+                    getStripeToken(cardForm.getCardNumber(), cardForm.getExpirationMonth(), cardForm.getExpirationYear(), cardForm.getCvv());
+                } else {
+                    cardForm.validate();
+                }
+            }
+        });
+    }
+
+    private void getStripeToken(String cardNumber, String expirationMonth, String expirationYear, String cvv) {
+        Card card = new Card(cardNumber, Integer.parseInt(expirationMonth), Integer.parseInt(expirationYear), cvv);
+        Stripe stripe = null;
+        try {
+            stripe = new Stripe(PUBLISHABLE_KEY);
+            stripe.createToken(
+                    card,
+                    new TokenCallback() {
+                        public void onSuccess(Token token) {
+                            Toast.makeText(AddPayment.this, token.getId(), Toast.LENGTH_SHORT).show();
+                            // sendToServer(token.getId());
+                        }
+
+                        public void onError(Exception error) {
+                            // Show localized error message
+                            Toast.makeText(AddPayment.this, error.getLocalizedMessage(),
+                                    Toast.LENGTH_LONG
+                            ).show();
+                        }
+                    }
+            );
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+        }
     }
 }
