d11cd4028362020c86938f1593e7509ac2869899
Basil <basiltalias92@gmail.com>
2016-11-25 02:56:12 -0500

    Added another test case Hash Password

---
+++ src/test/java/matrians/instapaysam/Schemas/UserTest.java
@@ -0,0 +1,17 @@
+package matrians.instapaysam.Schemas;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by basil on 2016-11-25.
+ */
+public class UserTest {
+    @Test
+    public void hashPassword() throws Exception {
+
+
+    }
+
+}
